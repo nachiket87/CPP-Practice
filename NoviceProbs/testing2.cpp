cda5fc54250605b59fa5a13eb5e08cb9436aa563
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-16 18:11:47 -0500

    old ones

---
+++ NoviceProbs/testing2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cctype>
+#include<unordered_set>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        cin >> x;
+        
+    }
+
+
+}
